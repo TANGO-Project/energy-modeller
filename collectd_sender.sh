577bf5946740d493cf8ec44f4d82f9b0143841a7
Richard Kavanagh <r_kav_0000@hotmail.com>
2017-07-19 14:40:41 +0100

    Updating the collectd scraping script.

--- collectd_sender.sh
+++ collectd_sender.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#Note this script works as part of collectd's exec functionality. This script is run and it scrapes from the output of the watt meter emulator.
 
  INTERVAL="${COLLECTD_INTERVAL:-20}"
  LOGFILE="/home_nfs/home_kavanagr/energymodeller/energy-modeller/energy-modeller-app-output.log"
- HOST_COUNT=5
 
  while sleep "$INTERVAL"; do
-   tail -n $HOST_COUNT $LOGFILE | awk '{split($0,values," "); printf "PUTVAL \""; printf values[1] ".bullx"; printf "/"; printf values[2]; printf "/"; printf values[2]; printf "\" interval=20 N:"; print values[3]}'
+   tail -n $APP_COUNT $LOGFILE | awk '{split($0,values," "); printf "PUTVAL \""; printf values[1] ".bullx"; printf "/"; printf values[2]; printf "/"; printf values[2]; printf "\" interval=20 N:"; print values[3]}'
  done
