d23b3202daf0b803628fd04299c78f4c8b6b2e97
Richard Kavanagh <scsrek@leeds.ac.uk>
2018-04-18 16:31:39 +0100

    Adding new comparators for applications

---
+++ src/main/java/eu/tango/energymodeller/types/energyuser/comparators/ApplicationOnHostId.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2018 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * This is being developed for the TANGO Project: http://tango-project.eu 
+ * 
+ */
+package eu.tango.energymodeller.types.energyuser.comparators;
+
+import eu.tango.energymodeller.types.energyuser.ApplicationOnHost;
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * This ranks applications on host by their id value. Normally the id value 
+ * increments for every new application, therefore this is sorting by age.
+ * @author Richard Kavanagh
+ */
+public class ApplicationOnHostId implements Comparator<ApplicationOnHost>, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public int compare(ApplicationOnHost o1, ApplicationOnHost o2) {
+        return Integer.valueOf(o1.getId()).compareTo(o2.getId());
+    }
+    
+}
---
+++ src/main/java/eu/tango/energymodeller/types/energyuser/comparators/ApplicationOnHostName.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2018 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * This is being developed for the TANGO Project: http://tango-project.eu 
+ * 
+ */
+package eu.tango.energymodeller.types.energyuser.comparators;
+
+import eu.tango.energymodeller.types.energyuser.ApplicationOnHost;
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * This ranks applications on host by their name.
+ * @author Richard Kavanagh
+ */
+public class ApplicationOnHostName implements Comparator<ApplicationOnHost>, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public int compare(ApplicationOnHost o1, ApplicationOnHost o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+    
+}
