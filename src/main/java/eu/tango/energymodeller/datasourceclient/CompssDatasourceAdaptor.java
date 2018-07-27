a93a3d7f0d365e14ef9c07ec2f3cdc6ddd42a3b4
Richard Kavanagh <r_kav_0000@hotmail.com>
2018-07-27 10:53:11 +0100

    Adding correct guard during new resource creation.

--- src/main/java/eu/tango/energymodeller/datasourceclient/CompssDatasourceAdaptor.java
+++ src/main/java/eu/tango/energymodeller/datasourceclient/CompssDatasourceAdaptor.java
@@ -288,13 +288,11 @@ public class CompssDatasourceAdaptor implements HostDataSource, ApplicationDataS
             JSONObject resourceInfo = compssState.getJSONObject(RESOURCE_INFO);
             List<CompssResource> resourceListing = CompssResource.getCompssResouce(resourceInfo);
             for (CompssResource resource : resourceListing) {
-                if (resource.getHostname().isEmpty() || resource.getHostname().replaceAll("[^0-9]", "").isEmpty()) {
+                if (resource.getHostname().contains("requested new VM")) {
                     /**
                      * This avoids parsing errors at the point a new resource is 
                      * added to the job, but before the host is fully detected.
-                     */
-                    Logger.getLogger(CompssDatasourceAdaptor.class.getName()).log(Level.WARNING, 
-                            "Host {0} not correctly detected", resource.getHostname());  
+                     */ 
                     continue;
                 }
                 Host host = new Host(Integer.parseInt(
@@ -372,6 +370,13 @@ public class CompssDatasourceAdaptor implements HostDataSource, ApplicationDataS
             JSONObject resourceInfo = compssState.getJSONObject(RESOURCE_INFO);
             List<CompssResource> resourceListing = CompssResource.getCompssResouce(resourceInfo);
             for (CompssResource resource : resourceListing) {
+                if (resource.getHostname().contains("requested new VM")) {
+                    /**
+                     * This avoids parsing errors at the point a new resource is 
+                     * added to the job, but before the host is fully detected.
+                     */ 
+                    continue;
+                }                
                 Host host = new Host(Integer.parseInt(
                                 resource.getHostname().replaceAll("[^0-9]", "")), 
                                 resource.getHostname());
